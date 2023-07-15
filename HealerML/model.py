94dd79c854e2950945b0105c01b48a96f6bcc8e7
我们叫啥名字呢 <940692727@qq.com>
2023-07-15 23:55:16 +0800

    feat: add early-stopping & fig

--- HealerML/model.py
+++ HealerML/model.py
@@ -57,13 +57,15 @@ class MLP(nn.Module):
         self.fc4 = nn.Linear(hidden_size, num_classes)
         self.activation = nn.LeakyReLU()
 
-    def forward(self, x):
+    def forward(self, x, return_features=False):
         out = self.fc1(x)
         out = self.activation(out)
         out = self.fc2(out)
         out = self.activation(out)
         out = self.fc3(out)
         out = self.activation(out)
+        if return_features == True:
+            return out.detach().cpu().numpy()
         out = self.fc4(out)
         return out
 
@@ -77,14 +79,13 @@ class EarlyStopping:
         self.early_stop = False
         self.val_loss_min = np.Inf
         self.delta = delta
-        self.best_val_features = None  # 添加这一行来保存最优的val_features
 
-    def __call__(self, val_loss, model, val_features, path):  # 在这里添加val_features参数
+    def __call__(self, val_loss, model):
         score = -val_loss
 
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, val_features, path)  # 在这里传入val_features参数
+            self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -92,16 +93,14 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model, val_features, path)  # 在这里传入val_features参数
+            self.save_checkpoint(val_loss, model)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model, val_features, path):  # 在这里添加val_features参数
+    def save_checkpoint(self, val_loss, model):
         if self.verbose:
             print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), path)
+        torch.save(model.state_dict(), 'checkpoint.pt')
         self.val_loss_min = val_loss
-        self.best_val_features = val_features  # 在这里保存最优的val_features
-
 
 def train(input_path, file_basic_path, output_path):
     global input_size_global, model_path
@@ -134,7 +133,7 @@ def train(input_path, file_basic_path, output_path):
     patience = 5  # patience for early stopping
     best_loss = np.inf
     stop_counter = 0
-    early_stopping = EarlyStopping(patience=7, verbose=True)
+    early_stopping = EarlyStopping(patience=5, verbose=True)
 
     for epoch in range(num_epochs):
         permutation = torch.randperm(X_train.size()[0])
@@ -152,15 +151,7 @@ def train(input_path, file_basic_path, output_path):
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        # Check for early stopping
-        if loss_test < best_loss:
-            best_loss = loss_test
-            stop_counter = 0
-        else:
-            stop_counter += 1
-            if stop_counter >= patience:
-                print("Early stopping")
-                break
+
 
         # 每个epoch打印损失
         print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}, Validate Loss: {loss_test.item():.4f}")
@@ -174,6 +165,11 @@ def train(input_path, file_basic_path, output_path):
             _, predicted = torch.max(outputs.data, 1)
             accuracy = (predicted == y_test).sum().item() / y_test.size(0)
             print(f"Test Accuracy: {accuracy:.4f}")
+            val_loss = criterion(outputs,y_test)
+            early_stopping(val_loss,model)
+            if early_stopping.early_stop:
+                print("early stop")
+                break
 
     # 在测试集上进行评估
     with torch.no_grad():
@@ -184,16 +180,18 @@ def train(input_path, file_basic_path, output_path):
     label_count_dict = [0, 0, 0, 0, 0, 0]
     for value in data['label']:
         label_count_dict[value] += 1
-    tsne = TSNE(n_components=2, random_state=0)
-    X_test_tsne = tsne.fit_transform(X_test)
+    val_features = model(X_test, return_features=True)
+    val_features_tsne = TSNE(n_components=2, random_state=33, init='pca',
+                                         learning_rate='auto').fit_transform(
+                    val_features)
     font = {"color": "darkred",
             "size": 13,
             "family": "serif"}
 
     plt.style.use("dark_background")
-    plt.figure(figsize=(9, 8))
+    plt.figure(figsize=(9, 8))  
 
-    plt.scatter(X_test_tsne[:, 0], X_test_tsne[:, 1], c=predicted, alpha=0.6,
+    plt.scatter(val_features_tsne[:, 0], val_features_tsne[:, 1], c=y_test.cpu().numpy(), alpha=0.6,
                 cmap=plt.cm.get_cmap('rainbow', num_classes))
     plt.title("t-SNE", fontdict=font)
     cbar = plt.colorbar(ticks=range(num_classes))
@@ -201,6 +199,23 @@ def train(input_path, file_basic_path, output_path):
     plt.clim(-0.5, num_classes - 0.5)
     plt.tight_layout()
     plt.savefig(file_basic_path + f'_tsne.png', dpi=300)
+    # tsne = TSNE(n_components=2, random_state=0)
+    # X_test_tsne = tsne.fit_transform(X_test)
+    # font = {"color": "darkred",
+    #         "size": 13,
+    #         "family": "serif"}
+
+    # plt.style.use("dark_background")
+    # plt.figure(figsize=(9, 8))
+
+    # plt.scatter(X_test_tsne[:, 0], X_test_tsne[:, 1], c=predicted, alpha=0.6,
+    #             cmap=plt.cm.get_cmap('rainbow', num_classes))
+    # plt.title("t-SNE", fontdict=font)
+    # cbar = plt.colorbar(ticks=range(num_classes))
+    # cbar.set_label(label='Class label', fontdict=font)
+    # plt.clim(-0.5, num_classes - 0.5)
+    # plt.tight_layout()
+    # plt.savefig(file_basic_path + f'_tsne.png', dpi=300)
 
     result = {"train_losses": train_loss_list, "val_losses": test_loss_list, "label_counts": label_count_dict}
     torch.save(model.state_dict(), file_basic_path + '_model.pth')
