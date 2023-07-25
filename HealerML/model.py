880d6d6aa68704fac23f45366c25850e9b6ca63e
dekrt <dekrt@qq.com>
2023-07-25 23:07:37 +0800

    refactor: modify code structure

--- HealerML/model.py
+++ HealerML/model.py
@@ -1,3 +1,4 @@
+# import necessary libraries
 import json
 import warnings
 import numpy as np
@@ -12,15 +13,19 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+
 warnings.filterwarnings('ignore')
 
+# define global variables
 input_size_global = 0
 model_path = ''
 
+
+# function to preprocess the data before training
 def preprocess(data):
-    # 去掉重复值
+    # remove duplicates
     data.drop_duplicates(inplace=True)
-    # 填充缺失值
+    # fill missing values
     imputer = IterativeImputer(random_state=0)
     data_imputed = imputer.fit_transform(data)
     data_imputed = pd.DataFrame(data_imputed, columns=data.columns)
@@ -28,22 +33,26 @@ def preprocess(data):
     y = data_imputed['label']
     columns_to_drop = ['label']
     data_imputed = data_imputed.drop(columns_to_drop, axis=1)
-    # 标准化
+    # standardize the data
     scaler = StandardScaler()
     data_standerd = scaler.fit_transform(data_imputed)
     return data_standerd, y
 
+
+# function to preprocess the data before testing
 def test_preprocess(data):
-    # 填充缺失值
+    # fill missing values
     imputer = IterativeImputer(random_state=0)
     data_imputed = imputer.fit_transform(data)
     data_imputed = pd.DataFrame(data_imputed, columns=data.columns)
     data_imputed = data_imputed.dropna()
-    # 标准化
+    # standardize the data
     scaler = StandardScaler()
     data_standerd = scaler.fit_transform(data_imputed)
     return data_standerd
 
+
+# define the MLP model
 class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(MLP, self).__init__()
@@ -60,12 +69,13 @@ class MLP(nn.Module):
         out = self.activation(out)
         out = self.fc3(out)
         out = self.activation(out)
-        if return_features == True:
+        if return_features:
             return out.detach().cpu().numpy()
         out = self.fc4(out)
         return out
 
 
+# define the EarlyStopping class
 class EarlyStopping:
     def __init__(self, patience=7, verbose=False, delta=0):
         self.patience = patience
@@ -98,8 +108,11 @@ class EarlyStopping:
         torch.save(model.state_dict(), 'checkpoint.pt')
         self.val_loss_min = val_loss
 
-def train(input_path, file_basic_path, output_path):
+
+# function to train the model
+def train(file_basic_path):
     global input_size_global, model_path
+    input_path = file_basic_path + '.csv'
     train_loss_list = []
     test_loss_list = []
     data = pd.read_csv(input_path)
@@ -107,22 +120,21 @@ def train(input_path, file_basic_path, output_path):
     kmeans_smote = KMeansSMOTE(cluster_balance_threshold=0.064, random_state=42)
     X_train, y_train = kmeans_smote.fit_resample(X_train, y_train)
     X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-    # -----------------------------MLP-----------------------------------#
-    # 转换为张量
+    # convert to tensors
     X_train = torch.Tensor(X_train)
     X_test = torch.Tensor(X_test)
     y_train = torch.LongTensor(y_train.values)
     y_test = torch.LongTensor(y_test.values)
-    input_size = X_train.shape[1]  # 输入特征的维度
+    input_size = X_train.shape[1]  # input feature dimensions
     input_size_global = X_train.shape[1]
-    hidden_size = 200  # 隐藏层的大小
-    num_classes = 6  # 类别数量
+    hidden_size = 200  # size of the hidden layer
+    num_classes = 6  # number of classes
     model = MLP(input_size, hidden_size, num_classes)
 
-    # 定义损失函数和优化器
+    # define the loss function and the optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    # 训练模型
+    # train the model
     num_epochs = 100
     batch_size = 30
     early_stopping = EarlyStopping(patience=5, verbose=True)
@@ -133,37 +145,35 @@ def train(input_path, file_basic_path, output_path):
             indices = permutation[i:i + batch_size]
             batch_X, batch_y = X_train[indices], y_train[indices]
 
-            # 前向传播
+            # forward propagation
             outputs = model(batch_X)
             loss = criterion(outputs, batch_y)
             predict = model(X_test)
             loss_test = criterion(predict, y_test)
 
-            # 反向传播和优化
+            # backward propagation and optimization
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-
-        # 每个epoch打印损失
+        # print the loss at each epoch
         print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}, Validate Loss: {loss_test.item():.4f}")
         train_loss_list.append(loss.item())
         test_loss_list.append(loss_test.item())
 
-        # early_stopping(loss.item(), model, val_features, 'checkpoint.pt')
-        # 在测试集上进行评估
+        # evaluate on the test set
         with torch.no_grad():
             outputs = model(X_test)
             _, predicted = torch.max(outputs.data, 1)
             accuracy = (predicted == y_test).sum().item() / y_test.size(0)
             print(f"Test Accuracy: {accuracy:.4f}")
-            val_loss = criterion(outputs,y_test)
-            early_stopping(val_loss,model)
+            val_loss = criterion(outputs, y_test)
+            early_stopping(val_loss, model)
             if early_stopping.early_stop:
                 print("early stop")
                 break
 
-    # 在测试集上进行评估
+    # evaluate on the test set
     with torch.no_grad():
         outputs = model(X_test)
         _, predicted = torch.max(outputs.data, 1)
@@ -174,14 +184,14 @@ def train(input_path, file_basic_path, output_path):
         label_count_dict[value] += 1
     val_features = model(X_test, return_features=True)
     val_features_tsne = TSNE(n_components=2, random_state=33, init='pca',
-                                         learning_rate='auto').fit_transform(
-                    val_features)
+                             learning_rate='auto').fit_transform(
+        val_features)
     font = {"color": "darkred",
             "size": 13,
             "family": "serif"}
 
     plt.style.use("dark_background")
-    plt.figure(figsize=(9, 8))  
+    plt.figure(figsize=(9, 8))
 
     plt.scatter(val_features_tsne[:, 0], val_features_tsne[:, 1], c=y_test.cpu().numpy(), alpha=0.6,
                 cmap=plt.cm.get_cmap('rainbow', num_classes))
@@ -196,24 +206,27 @@ def train(input_path, file_basic_path, output_path):
     model_path = file_basic_path + '_model.pth'
     return result
 
-def test(input_path, file_basic_path, output_path):
+
+# function to test the model
+def test(file_basic_path):
     global input_size_global, model_path
+    input_path = file_basic_path + '.csv'
     data = pd.read_csv(input_path)
     data = test_preprocess(data)
-    input_size = input_size_global  # 输入特征的维度
-    hidden_size = 200  # 隐藏层的大小
-    num_classes = 6  # 类别数量
+    input_size = input_size_global  # input feature dimensions
+    hidden_size = 200  # size of the hidden layer
+    num_classes = 6  # number of classes
     model = MLP(input_size, hidden_size, num_classes)
     model.load_state_dict(torch.load(model_path))
     with torch.no_grad():
         data = torch.Tensor(data)
         outputs = model(data)
         _, predicted = torch.max(outputs.data, 1)
-    
+
     label_count_list = [0, 0, 0, 0, 0, 0]
     for value in predicted:
         label_count_list[value] += 1
-    
+
     result_dict = {}
     predicted_new = predicted.tolist()
     for i, value in enumerate(predicted_new, start=1):
@@ -223,14 +236,14 @@ def test(input_path, file_basic_path, output_path):
     result = {"label_counts": label_count_list}
     val_features = model(data, return_features=True)
     val_features_tsne = TSNE(n_components=2, random_state=33, init='pca',
-                                         learning_rate='auto').fit_transform(
-                    val_features)
+                             learning_rate='auto').fit_transform(
+        val_features)
     font = {"color": "darkred",
             "size": 13,
             "family": "serif"}
 
     plt.style.use("dark_background")
-    plt.figure(figsize=(9, 8))  
+    plt.figure(figsize=(9, 8))
 
     plt.scatter(val_features_tsne[:, 0], val_features_tsne[:, 1], c=predicted.cpu().numpy(), alpha=0.6,
                 cmap=plt.cm.get_cmap('rainbow', num_classes))
--- HealerML/views.py
+++ HealerML/views.py
@@ -1,93 +1,115 @@
+# import necessary libraries
 from django.shortcuts import render, HttpResponse
 from django.core.files.storage import FileSystemStorage
 from django.http import FileResponse
-import os, datetime
+import os
+import datetime
 from . import model
 
-# Change the extensions to accept only CSV files
+# define global variables
+# list of allowed extensions for the data file
 extensions = ['.csv']
+# boolean variable to check if it's the training phase or not
 is_training = True
+# path to the saved model
 model_path = ''
 
 
+# function to handle the main page of the web application
 def index(request):
+    # use global variables declared above
     global is_training, model_path
+
+    # if the request method is POST and there is a file attached
     if request.method == 'POST' and request.FILES['data']:
         data_file = request.FILES['data']
         ext = os.path.splitext(data_file.name)[1]
+
+        # if the file has a correct extension
         if ext.lower() in extensions:
             fs = FileSystemStorage()
             filename = fs.save(data_file.name, data_file)
             uploaded_file_url = fs.url(filename)
             file_name = uploaded_file_url.split("/")[2]
-
-            input_path = os.getcwd() + uploaded_file_url
             file_basic_path = os.getcwd() + "/uploads/" + file_name.split(".")[0]
-            output_path = os.getcwd() + "/uploads/" + file_name.split(".")[0] + "_predicted.json"
-            if is_training:  # Train the model if it doesn't exist.
-                result = model.train(input_path, file_basic_path, output_path)
+
+            # if it's the training phase
+            if is_training:
+                result = model.train(file_basic_path)
                 uploads_path = uploaded_file_url.split(".")[0]
                 model_path = file_basic_path + "_model.pth"
                 model_download_link = '/download_model/' + os.path.basename(model_path)
-                is_training = False  # Next time, we will do prediction.
-                print('No. 2: ', is_training)
-                return render(request, 'HealerML/index.html', {"model_download_link": model_download_link,
-                                                               "uploads_path": uploads_path,
-                                                               "file_basic_path": file_basic_path,
-                                                               "train_loss": result["train_losses"],
-                                                               "val_loss": result["val_losses"],
-                                                               "label_counts": result["label_counts"]
-                                                               })
+                is_training = False
+                return render(request, 'HealerML/index.html', {
+                    "model_download_link": model_download_link,
+                    "uploads_path": uploads_path,
+                    "file_basic_path": file_basic_path,
+                    "train_loss": result["train_losses"],
+                    "val_loss": result["val_losses"],
+                    "label_counts": result["label_counts"]
+                })
 
-            else:  # Load the model and predict if it exists.
-                result = model.test(input_path, file_basic_path, output_path)
+            # if it's the testing phase
+            else:
+                result = model.test(file_basic_path)
                 uploads_path = uploaded_file_url.split(".")[0]
                 model_download_link = '/download_model/' + os.path.basename(model_path)
-                download_link = '/download/' + os.path.basename(output_path)
-                is_training = True  # Next time, we will do training.
-                print(result['label_counts'])
-                return render(request, 'HealerML/index.html', {"model_download_link": model_download_link,
-                                                               "uploads_path": uploads_path,
-                                                               "file_basic_path": file_basic_path,
-                                                               "download_link": download_link,
-                                                               "label_counts": result["label_counts"]
-                                                               })
+                download_link = '/download/' + os.path.basename(file_basic_path + '_predicted.json')
+                is_training = True
+                return render(request, 'HealerML/index.html', {
+                    "model_download_link": model_download_link,
+                    "uploads_path": uploads_path,
+                    "file_basic_path": file_basic_path,
+                    "download_link": download_link,
+                    "label_counts": result["label_counts"]
+                })
+
         else:
             return HttpResponse("Only Allowed extensions are {}".format(extensions))
+
+    # if the request method is not POST or there is no file attached
     return render(request, 'HealerML/index.html')
 
+
+# function to handle the data endpoint of the web application
 def data(request):
+    # if the request method is POST and there is some data attached
     if request.method == 'POST' and request.POST['data']:
-        data_file = request.POST['data']
         data_file_name = datetime.datetime.now().strftime("%Y%b%d%H%M%S%f") + ".csv"
         data_file_path = os.getcwd() + "/uploads/" + data_file_name
 
+        # write the data to a file
         with open(data_file_path, "wb") as f:
             f.write(data)
-
-        input_path = os.getcwd() + "/uploads/" + data_file_name
-        output_path = os.getcwd() + "/uploads/" + data_file_name.split(".")[0] + "_predicted.csv"
-
-
-        model.preprocess(input_path, input_path)
-        result = model.train(input_path, output_path)
         data_file_path = "/uploads/" + data_file_name.split(".")[0] + "_predicted.csv"
+
+        # return the path to the file
         return HttpResponse(request.get_host() + data_file_path)
 
 
+# function to handle the download endpoint of the web application
 def download(request, file_name):
     file_path = os.getcwd() + "/uploads/" + file_name
+
+    # if the file exists, return it as a response
     if os.path.exists(file_path):
         response = FileResponse(open(file_path, 'rb'), content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_name)
         return response
+
+    # if the file doesn't exist, return an error message
     else:
         return HttpResponse("Sorry file not found.")
 
 
+# function to handle the download_model endpoint of the web application
 def download_model(request, file_name):
     file_path = os.getcwd() + "/uploads/" + file_name
+
+    # if the model file exists, return it as a response
     if os.path.exists(file_path):
         return FileResponse(open(file_path, 'rb'), as_attachment=True, filename=file_name)
+
+    # if the model file doesn't exist, return an error message
     else:
         return HttpResponse("Sorry, model file not found.")
